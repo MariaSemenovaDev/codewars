ca9829d1db76ea1831a3268234f112e00ab52c3f
Maria Semenova <semenovamk96@mail.ru>
2024-10-24 15:33:43 +0300

    returning strings

--- 8kyu/main.js
+++
@@ -1,7 +0,0 @@
-function greet(name){
-    return `Hello, ${name} how are you doing today?`
-  }
-
-  let a = greet("Alex")
-  console.log(a);
-  
---
+++ 8kyu/returning-strings.js
@@ -0,0 +1,12 @@
+// DISCRIPTION: Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+
+
+// SOLUTION:
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+  }
+
+  // ПРОВЕРКА В БРАУЗЕРЕ:
+  // let a = greet("Alex")
+  // console.log(a);
