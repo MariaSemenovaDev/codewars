d512831a4261854512c5f6afbfd221af49331cfa
Maria Semenova <semenovamk96@mail.ru>
2025-05-15 17:47:34 +0300

    new commit

---
+++ 6 kyu/Shorten Me/main.js
@@ -0,0 +1,12 @@
+// Shorten Me: Array Declaration IX (Variable Types)
+// Task
+// You have to write an array() function returning an array object with elements as follow:
+//
+//     [ 'array', 'boolean', 'function', 'number', 'object', 'string', 'undefined' ]
+// Coding Limitation:
+//     Less than 60 characters
+//
+// |||| |---|---:| |Previous: Part VIII (Musical Scales)|Next: Part X (O-I-II)|
+
+
+array=a=_=>['array',...[!1,a,1,{},'',_].map(v=>typeof v)]
