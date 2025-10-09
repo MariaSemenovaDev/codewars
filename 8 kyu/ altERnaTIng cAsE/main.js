70500717071a66bcccccad334fb766b02bd75e01
Maria Semenova <semenovamk96@mail.ru>
2025-10-09 15:07:46 +0300

    new commit

--- 8 kyu/ altERnaTIng cAsE/main.js
+++ 8 kyu/ altERnaTIng cAsE/main.js
@@ -6,10 +6,10 @@
 // "hello WORLD".toAlternatingCase() === "HELLO world"
 // "HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
 // "12345".toAlternatingCase()       === "12345"                   // Non-alphabetical characters are unaffected
-// "1a2b3c4d5e".toAlternatingCase()  === "1A2B3C4D5E"
 // "String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
 // As usual, your function/method should be pure, i.e. it should not mutate the original string.
 //
+
 
 String.prototype.toAlternatingCase = function () {
     return this.replace(/[a-z]/gi, char =>
