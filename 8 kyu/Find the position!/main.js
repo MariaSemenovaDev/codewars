9e7fe8e9e96fba383641b89a3ff1d1221da35f4d
Maria Semenova <semenovamk96@mail.ru>
2025-10-17 21:20:12 +0300

    8 kyu

---
+++ 8 kyu/Find the position!/main.js
@@ -0,0 +1,12 @@
+// When provided with a letter, return its position in the alphabet.
+//
+// Input :: "a"
+//
+// Output :: "Position of alphabet: 1"
+//
+// Note: Only lowercased English letters are tested
+
+function position(letter){
+    const position = letter.charCodeAt(0) - 96;
+    return `Position of alphabet: ${position}`;
+}
